d403b5e4c67efcb03278993f20236839c8f78c7d
TYJ <420785022@qq.com>
2018-12-11 10:58:05 +0800

    使用mock

--- src/main.js
+++ src/main.js
@@ -6,7 +6,11 @@ import router from './router'
 // 引用axios，并设置基础URL为后端服务api地址
 var axios = require('axios')
 axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.baseURL = 'http://localhost:8081/web_api'
+//访问后端方式：
+// 1.使用web api
+//axios.defaults.baseURL = 'http://localhost:8081/web_api'
+// 2.使用mock 
+require('./mock');
 // 将API方法绑定到全局
 Vue.prototype.$axios = axios
 axios.defaults.withCredentials = true
@@ -19,7 +23,6 @@ Vue.use(ElementUI)
 //引入公共JS
 import common from "./components/comm/common.js";
 Vue.prototype.common = common
-
 //引入echarts
 import echarts from 'echarts'
 Vue.prototype.$echarts = echarts
